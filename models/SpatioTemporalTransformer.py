417a7ae00668cee0cc75dd3e4f973a71e8757d3e
Tim <ethosscream@yahoo.com>
2023-11-26 21:02:45 -0500

    Add per-joint positional encoding

--- models/SpatioTemporalTransformer.py
+++ models/SpatioTemporalTransformer.py
@@ -1,9 +1,36 @@
+import numpy as np
 import torch
 import torch.nn as nn
 
 
-class JointEmbedder(nn.Module):
-    def __init__(self, num_joints, joint_dim, embedding_dim):
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model, dropout=0.5, max_len=5000):
+        """
+        Initializes a positional embedding layer for each joint.
+        """
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(
+            torch.arange(0, d_model, 2).float() * (-np.log(10000.0) / d_model)
+        )
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0)
+        self.register_buffer("pe", pe)
+
+    def forward(self, x):
+        """
+        Implements the forward pass on the embedded joints.
+        """
+        x = x + self.pe[:, :x.shape[1], :]
+        return self.dropout(x)
+
+
+class JointEmbedding(nn.Module):
+    def __init__(self, num_joints, joint_dim, embedding_dim, dropout):
         """
         Initializes a linear embedding layer for each joint.
         """
@@ -12,6 +39,8 @@ class JointEmbedder(nn.Module):
         self.joint_dim = joint_dim
         self.embedding_dim = embedding_dim
         self.embeddings = nn.ModuleList([nn.Linear(joint_dim, embedding_dim) for _ in range(num_joints)])
+        self.positional_embeddings = nn.ModuleList(
+            [PositionalEncoding(embedding_dim, dropout=dropout, max_len=300) for _ in range(num_joints)])
 
     def forward(self, src_seqs):
         """
@@ -31,13 +60,15 @@ class JointEmbedder(nn.Module):
         Returns:
             A Tensor with the final dimension embedded.
         """
-        joint_embeddings = []
+        pos_embeddings = []
         for i in range(self.num_joints):
             j_i = src_seqs[:, :, i * self.joint_dim:(i+1) * self.joint_dim]
-            joint_embeddings.append(self.embeddings[i](j_i))
+            e_i = self.embeddings[i](j_i)
+            p_i = self.positional_embeddings[i](e_i)
+            pos_embeddings.append(p_i)
 
-        embedded_seqs = torch.cat(joint_embeddings, dim=-1)
-        return embedded_seqs
+        pos_seqs = torch.cat(pos_embeddings, dim=-1)
+        return pos_seqs
 
 
 class SpatioTemporalAttentionBlock(nn.Module):
@@ -45,7 +76,7 @@ class SpatioTemporalAttentionBlock(nn.Module):
 
 
 class SpatioTemporalTransformer(nn.Module):
-    def __init__(self, num_joints, joint_dim, input_dim, embedding_dim):
+    def __init__(self, num_joints, joint_dim, input_dim, embedding_dim, embedding_dropout):
         """
         """
         super().__init__()
@@ -53,15 +84,15 @@ class SpatioTemporalTransformer(nn.Module):
         self.joint_dim = joint_dim
         self.input_dim = input_dim
         self.embedding_dim = embedding_dim
+        self.embedding_dropout = embedding_dropout
 
         # Define Modules
-        print(embedding_dim * num_joints, embedding_dim * joint_dim)
-        self.joint_embedder = JointEmbedder(num_joints, joint_dim, embedding_dim)
-        self.output = nn.Linear(num_joints * embedding_dim, num_joints * joint_dim)  # TODO: Delete this placeholder
+        self.joint_embedding = JointEmbedding(num_joints, joint_dim, embedding_dim, embedding_dropout)
+        self.output = nn.Linear(num_joints * embedding_dim, num_joints * joint_dim)
 
     def forward(self, src_seqs):
         """
         """
-        embeddings_seqs = self.joint_embedder(src_seqs)
-        print(src_seqs.shape, embeddings_seqs.shape)
+        embeddings_seqs = self.joint_embedding(src_seqs)
+        print(f"src_seqs.shape={src_seqs.shape}", f"embeddings_seqs.shape={embeddings_seqs.shape}")
         return self.output(embeddings_seqs)
