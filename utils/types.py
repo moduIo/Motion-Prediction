26a60b3ab941e08c5e1969ff4eadcd8f3173f63e
Johnny Evans <jrevansjr2209@gmail.com>
2023-11-28 22:54:05 -0600

    adding files

--- utils/types.py
+++ utils/types.py
@@ -3,8 +3,9 @@ from enum import Enum
 
 class ModelEnum(Enum):
     SPATIO_TEMPORAL_TRANSFORMER = "spatio-temporal-transformer"
-
+    BIDIRECTIONAL_TRANSFORMER = "bi-drectional-transformer"
 
 class TargetEnum(Enum):
     AUTO_REGRESSIVE = "auto-regressive"
+    PRE_TRAIN = "pre-train"
     DEFAULT = "default"
