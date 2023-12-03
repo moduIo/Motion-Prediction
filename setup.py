d59f0d97b87c70932b4f704aa3d27414188827b9
Johnny Evans <jrevansjr2209@gmail.com>
2023-12-03 13:29:31 -0600

    setup py file add

---
+++ setup.py
@@ -0,0 +1,36 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+
+from setuptools import find_packages, setup
+
+setup(
+    name="fairmotion",
+    version="0.0.4",
+    description="fairmotion is FAIR's library for human motion research",
+    url="https://github.com/facebookresearch/fairmotion",
+    author="FAIR Pittsburgh",
+    author_email="dgopinath@fb.com",
+    install_requires=[
+        "black",
+        "dataclasses",  # py3.6 backport required by human_body_prior
+        "matplotlib",
+        "numpy",
+        "pillow",
+        "pyrender==0.1.39",
+        "scikit-learn",
+        "scipy",
+        "torch",
+        "tqdm",
+        "pyglet==1.5.27",
+        "PyOpenGL==3.1.0",
+        "PyOpenGL-accelerate",
+        "body_visualizer @ git+https://github.com/nghorbani/body_visualizer.git@be9cf756f8d1daed870d4c7ad1aa5cc3478a546c",
+        "human_body_prior @ git+https://github.com/nghorbani/human_body_prior.git@0278cb45180992e4d39ba1a11601f5ecc53ee148",
+        "opencv-python"
+    ],
+    packages=find_packages(exclude=["tests"]),
+    classifiers=[
+        "License :: OSI Approved :: BSD License",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
